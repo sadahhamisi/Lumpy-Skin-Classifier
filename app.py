f32436c3c0012ff74de299af76b1605d9f018ab5
Sadah Hamisi <49056875+sadahhamisi@users.noreply.github.com>
2023-07-11 10:45:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,36 @@ model = tf.keras.models.load_model('CNN.h5')
 # Define the class names for predictions
 class_names = ["Healthy Cow", "Lumpy Cow"]
 
-    # Set Streamlit app configuration
+# Set Streamlit app configuration
 st.set_page_config(page_title='Disease Classification', layout='wide')
+# Define the login page
+def login_page():
+    st.sidebar.title("Login")
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+    login_button = st.sidebar.button("Login", key="login_button")
 
-# Rest of the code remains the same
-def Predict(model, img):
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-    predictions = model.predict(img_array)
-    predicted_class = class_names[np.argmax(predictions[0])]
-    confidence = round(100 * (np.max(predictions[0])), 2)
-    return predicted_class,
+    if login_button:
+        # Validate the username and password
+        if username == "your_username" and password == "your_password":
+            st.sidebar.success("Logged in successfully!")
+             # Addind a logout button
+            st.sidebar.button("Logout", on_click=logout, key="logout_button") 
+            st.sidebar.text("You are logged in as: " + username)
+            st.experimental_set_query_params(logged_in="true")  # Set query parameter
+            show_main_app()
+        else:
+            st.sidebar.error("Invalid username or password")
 
+# Define the logout function
+def logout():
+    st.experimental_set_query_params(logged_in="false")
+    st.sidebar.text("You have been logged out.")
+    st.sidebar.empty()  # Clear the sidebar content
+    st.sidebar.button("Login", on_click=login_page, key="login_button")  # Show login button
 
-# Define the main content of the app
-def main():
-    # Set app title and description
+# Define the main app content
+def show_main_app():
     st.title('Lumpy Skin Disease Classification')
     st.write('Upload an image to classify.')
 
@@ -47,11 +61,17 @@ def main():
         st.subheader('Prediction Result')
         st.write('Class:', predicted_class)
 
-
-
-        st.subheader('click the link to know more')
-        st.write('https://cow-disease-diagnosis.vercel.app/')
+        if predicted_class == "Lumpy Cow":
+            st.subheader('Click the link to know more')
+            st.write('https://cow-disease-diagnosis.vercel.app/')
 
 # Run the app
+def main():
+    # Check if the user is authenticated
+    if st.experimental_get_query_params().get("logged_in", ["false"])[0] == "true":
+        show_main_app()
+    else:
+        login_page()
+
 if __name__ == '__main__':
     main()
